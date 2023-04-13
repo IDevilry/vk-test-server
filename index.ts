dd1e8a9154cd82315164eab84f410c6225143ed8
Андрей <andrey.alekseevich.22@yandex.ru>
2023-04-13 13:33:48 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const PORT = process.env.PORT
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.json('Express + TS')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`)
+})
