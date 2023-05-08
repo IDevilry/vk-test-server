fd647f228b8bceb1159bccd3ae420bdac418e201
Андрей <andrey.alekseevich.22@yandex.ru>
2023-05-08 13:38:07 +0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -94,7 +94,13 @@ class App {
     this.app.use(express.static(path.resolve(__dirname, "public")));
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
-    this.app.use(fileupload());
+    this.app.use(
+      fileupload({
+        headers: {
+          "access-control-allow-origin": CLIENT_HOST,
+        },
+      })
+    );
   }
 
   private routes(): void {
